07e966c90f0b11cefa050010aa9b89114cc5123d
BrandonV18 <31927431+BrandonV18@users.noreply.github.com>
2017-10-12 08:42:13 -0400

    Add files via upload
    2.  Entering Flavor to eat

--- ice-cream.js
+++ ice-cream.js
@@ -1,2 +1,9 @@
 // Set a string variable to equal a value
 var flavor = prompt("Enter a flavor");
+
+if (flavor ==  "Mint" || flavor == "CottonCandy") {
+	alert("Here is your "+flavor+" ice cream boi later");
+	}
+else if (flavor == "Bananna"){
+    alert("Sorry fam we out");
+}
